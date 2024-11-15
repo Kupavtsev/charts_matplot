6d7426aed28e1ffa273e0958b89918be43593a2d
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2024-11-15 14:33:31 +0300

    first done

--- db_connect.py
+++ db_connect.py
@@ -7,7 +7,7 @@ import asyncpg
 
 dotenv.load_dotenv()
 
-assets = {'BTCUSDT':4, 'ARKMUSDT':15}
+assets = {'ACHUSDT':1, 'BTCUSDT':4, 'ARKMUSDT':15, 'LPTUSDT':3,'STORJUSDT':6,'WLDUSDT':7, 'AMBUSDT':12, 'KNCUSDT':9, 'LEVERUSDT':10, 'MKRUSDT':8, 'PENDLEUSDT':14, 'SPELLUSDT':13}
 
 async def get_data(asset):
     
@@ -21,7 +21,7 @@ async def get_data(asset):
 
     try:
         query = f"SELECT id, session_date, start_of_candle, price_open, price_high, price_low, price_close, volume, symbol_id FROM public.get_data_five_minutes\
-                WHERE symbol_id={assets[asset]} AND session_date = CURRENT_DATE - 1;"
+                WHERE symbol_id={assets[asset]} AND session_date = CURRENT_DATE;"
 
         rows = await conn.fetch(query)
         data = pd.DataFrame(rows)
@@ -56,7 +56,7 @@ class ConsLevels():
             cursor.execute(
                 # "SELECT id, session_date, start_of_candle, price_open, price_high, price_low, price_close, volume, ma7, ma14, ma28, symbol_id FROM public.get_data_five_minutes;"
                 f"SELECT id, symbol, session, cons_ysd_body_levels, cons_ysd_body_levels_neg, cons_ysd_body_levels_tr100, cons_ysd_body_levels_tr100_neg, series_neg, series_pos\
-                FROM public.ysd_body_levels WHERE symbol='{asset}' AND session = CURRENT_DATE - 1;"
+                FROM public.ysd_body_levels WHERE symbol='{asset}' AND session = CURRENT_DATE;"
                 )
             
             # Fetch all rows
@@ -84,6 +84,28 @@ arkm = ConsLevels()
 arkm.get_cons_levels('ARKMUSDT')
 ach = ConsLevels()
 ach.get_cons_levels('ACHUSDT')
+lpt = ConsLevels()
+lpt.get_cons_levels('LPTUSDT')
+
+
+storj = ConsLevels()
+storj.get_cons_levels('STORJUSDT')
+wld = ConsLevels()
+wld.get_cons_levels('WLDUSDT')
+amb = ConsLevels()
+amb.get_cons_levels('AMBUSDT')
+knc = ConsLevels()
+knc.get_cons_levels('KNCUSDT')
+
+lever = ConsLevels()
+lever.get_cons_levels('LEVERUSDT')
+mkr = ConsLevels()
+mkr.get_cons_levels('MKRUSDT')
+pendle = ConsLevels()
+pendle.get_cons_levels('PENDLEUSDT')
+spell = ConsLevels()
+spell.get_cons_levels('SPELLUSDT')
+
 
 if __name__ == '__main__':
 #    get_cons_levels('BTCUSDT')
--- two.py
+++ two.py
@@ -5,24 +5,45 @@ import yfinance as yf
 import time
 import asyncio
 
-from db_connect import get_data, btc, arkm, ach
+from db_connect import get_data,\
+                       btc, arkm, ach, lpt, storj, wld, amb, knc, lever, mkr, pendle, spell
 
 
 # Cons Levels
 cons_levels = {
     'BTCUSDT': [btc.cons_lev1, btc.cons_lev2, btc.cons_lev3, btc.cons_lev4],
-    'ARKMUSDT': [arkm.cons_lev1, arkm.cons_lev2, arkm.cons_lev3, arkm.cons_lev4],
     'ACHUSDT': [ach.cons_lev1, ach.cons_lev2, ach.cons_lev3, ach.cons_lev4],
+    'ARKMUSDT': [arkm.cons_lev1, arkm.cons_lev2, arkm.cons_lev3, arkm.cons_lev4],
+    'LPTUSDT': [lpt.cons_lev1, lpt.cons_lev2, lpt.cons_lev3, lpt.cons_lev4],
+    'STORJUSDT': [storj.cons_lev1, storj.cons_lev2, storj.cons_lev3, storj.cons_lev4],
+    'WLDUSDT': [wld.cons_lev1, wld.cons_lev2, wld.cons_lev3, wld.cons_lev4],
+    'AMBUSDT': [amb.cons_lev1, amb.cons_lev2, amb.cons_lev3, amb.cons_lev4],
+    'KNCUSDT': [knc.cons_lev1, knc.cons_lev2, knc.cons_lev3, knc.cons_lev4],
+    'LEVERUSDT': [lever.cons_lev1, lever.cons_lev2, lever.cons_lev3, lever.cons_lev4],
+    'MKRUSDT': [mkr.cons_lev1, mkr.cons_lev2, mkr.cons_lev3, mkr.cons_lev4],
+    'PENDLEUSDT': [pendle.cons_lev1, pendle.cons_lev2, pendle.cons_lev3, pendle.cons_lev4],
+    'SPELLUSDT': [spell.cons_lev1, spell.cons_lev2, spell.cons_lev3, spell.cons_lev4],
 }
 
 # Create a figure and axis
-fig, axs = plt.subplots(1, 3, figsize=(14, 5))
+fig, axs = plt.subplots(3, 4, figsize=(14, 5))
 
 # Initialize the plot
 def init():
-    axs[0].set_title('BTC')
-    axs[1].set_title('ARKM')
-    axs[2].set_title('ACH')
+    axs[0][0].set_title('BTC')
+    axs[0][1].set_title('ARKM')
+    axs[0][2].set_title('ACH')
+    axs[0][3].set_title('LPT')
+    
+    axs[1][0].set_title('STORJ')
+    axs[1][1].set_title('WLD')
+    axs[1][2].set_title('AMB')
+    axs[1][3].set_title('KNC')
+    
+    axs[2][0].set_title('LEVER')
+    axs[2][1].set_title('MKR')
+    axs[2][2].set_title('PENDLE')
+    axs[2][3].set_title('SPELL')
     return axs
 
 # Update function for animation
@@ -31,42 +52,132 @@ def update(frame):
     data1 = asyncio.run(get_data('BTCUSDT'))
     data2 = asyncio.run(get_data('ARKMUSDT'))
     data3 = asyncio.run(get_data('ACHUSDT'))
+    data4 = asyncio.run(get_data('LPTUSDT'))
+
+    data5 = asyncio.run(get_data('STORJUSDT'))
+    data6 = asyncio.run(get_data('WLDUSDT'))
+    data7 = asyncio.run(get_data('AMBUSDT'))
+    data8 = asyncio.run(get_data('KNCUSDT'))
+
+    data9 = asyncio.run(get_data('LEVERUSDT'))
+    data10 = asyncio.run(get_data('MKRUSDT'))
+    data11 = asyncio.run(get_data('PENDLEUSDT'))
+    data12 = asyncio.run(get_data('SPELLUSDT'))
     
 
     # Clear previous data
-    axs[0].cla()
-    axs[1].cla()
-    axs[2].cla()
+    axs[0][0].cla()
+    axs[0][1].cla()
+    axs[0][2].cla()
+    axs[0][3].cla()
+
+    axs[1][0].cla()
+    axs[1][1].cla()
+    axs[1][2].cla()
+    axs[1][3].cla()
+
+    axs[2][0].cla()
+    axs[2][1].cla()
+    axs[2][2].cla()
+    axs[2][3].cla()
 
     # Plot for BTC
-    axs[0].plot(data1.index, data1[6], label='BTC', color='blue')
-    axs[0].axhline(y=cons_levels['BTCUSDT'][0], color='red', linestyle='--', label='Level 1')
-    axs[0].axhline(y=cons_levels['BTCUSDT'][1], color='green', linestyle='--', label='Level 2')
-    axs[0].axhline(y=cons_levels['BTCUSDT'][2], color='red', linestyle='--', label='Level 3')
-    axs[0].axhline(y=cons_levels['BTCUSDT'][3], color='green', linestyle='--', label='Level 4')
+    axs[0][0].plot(data1.index, data1[6], label='BTC', color='blue')
+    axs[0][0].axhline(y=cons_levels['BTCUSDT'][0], color='red', linestyle='--', label='Level 1')
+    axs[0][0].axhline(y=cons_levels['BTCUSDT'][1], color='green', linestyle='--', label='Level 2')
+    axs[0][0].axhline(y=cons_levels['BTCUSDT'][2], color='red', linestyle='--', label='Level 3')
+    axs[0][0].axhline(y=cons_levels['BTCUSDT'][3], color='green', linestyle='--', label='Level 4')
     # axs[0].legend()
 
     # Plot for ARKM
-    axs[1].plot(data2.index, data2[6], label='arkm', color='orange')
-    axs[1].axhline(y=cons_levels['ARKMUSDT'][0], color='red', linestyle='--', label='Level 1')
-    axs[1].axhline(y=cons_levels['ARKMUSDT'][1], color='green', linestyle='--', label='Level 2')
-    axs[1].axhline(y=cons_levels['ARKMUSDT'][2], color='red', linestyle='--', label='Level 3')
-    axs[1].axhline(y=cons_levels['ARKMUSDT'][3], color='green', linestyle='--', label='Level 4')
+    axs[0][1].plot(data2.index, data2[6], label='arkm', color='orange')
+    axs[0][1].axhline(y=cons_levels['ARKMUSDT'][0], color='red', linestyle='--', label='Level 1')
+    axs[0][1].axhline(y=cons_levels['ARKMUSDT'][1], color='green', linestyle='--', label='Level 2')
+    axs[0][1].axhline(y=cons_levels['ARKMUSDT'][2], color='red', linestyle='--', label='Level 3')
+    axs[0][1].axhline(y=cons_levels['ARKMUSDT'][3], color='green', linestyle='--', label='Level 4')
     # axs[1].legend()
 
-    axs[2].plot(data3.index, data3[6], label='ach', color='orange')
-    axs[2].axhline(y=cons_levels['ACHUSDT'][0], color='red', linestyle='--', label='Level 1')
-    axs[2].axhline(y=cons_levels['ACHUSDT'][1], color='green', linestyle='--', label='Level 2')
-    axs[2].axhline(y=cons_levels['ACHUSDT'][2], color='red', linestyle='--', label='Level 3')
-    axs[2].axhline(y=cons_levels['ACHUSDT'][3], color='green', linestyle='--', label='Level 4')
+    axs[0][2].plot(data3.index, data3[6], label='ach', color='orange')
+    axs[0][2].axhline(y=cons_levels['ACHUSDT'][0], color='red', linestyle='--', label='Level 1')
+    axs[0][2].axhline(y=cons_levels['ACHUSDT'][1], color='green', linestyle='--', label='Level 2')
+    axs[0][2].axhline(y=cons_levels['ACHUSDT'][2], color='red', linestyle='--', label='Level 3')
+    axs[0][2].axhline(y=cons_levels['ACHUSDT'][3], color='green', linestyle='--', label='Level 4')
+    
+    axs[0][3].plot(data4.index, data4[6], label='lpt', color='orange')
+    axs[0][3].axhline(y=cons_levels['LPTUSDT'][0], color='red', linestyle='--', label='Level 1')
+    axs[0][3].axhline(y=cons_levels['LPTUSDT'][1], color='green', linestyle='--', label='Level 2')
+    axs[0][3].axhline(y=cons_levels['LPTUSDT'][2], color='red', linestyle='--', label='Level 3')
+    axs[0][3].axhline(y=cons_levels['LPTUSDT'][3], color='green', linestyle='--', label='Level 4')
+    
+    
+    
+    axs[1][0].plot(data5.index, data5[6], label='lpt', color='orange')
+    axs[1][0].axhline(y=cons_levels['STORJUSDT'][0], color='red', linestyle='--', label='Level 1')
+    axs[1][0].axhline(y=cons_levels['STORJUSDT'][1], color='green', linestyle='--', label='Level 2')
+    axs[1][0].axhline(y=cons_levels['STORJUSDT'][2], color='red', linestyle='--', label='Level 3')
+    axs[1][0].axhline(y=cons_levels['STORJUSDT'][3], color='green', linestyle='--', label='Level 4')
+    
+    axs[1][1].plot(data6.index, data6[6], label='lpt', color='orange')
+    axs[1][1].axhline(y=cons_levels['WLDUSDT'][0], color='red', linestyle='--', label='Level 1')
+    axs[1][1].axhline(y=cons_levels['WLDUSDT'][1], color='green', linestyle='--', label='Level 2')
+    axs[1][1].axhline(y=cons_levels['WLDUSDT'][2], color='red', linestyle='--', label='Level 3')
+    axs[1][1].axhline(y=cons_levels['WLDUSDT'][3], color='green', linestyle='--', label='Level 4')
+   
+    axs[1][2].plot(data7.index, data7[6], label='lpt', color='green')
+    axs[1][2].axhline(y=cons_levels['AMBUSDT'][0], color='red', linestyle='--', label='Level 1')
+    axs[1][2].axhline(y=cons_levels['AMBUSDT'][1], color='green', linestyle='--', label='Level 2')
+    axs[1][2].axhline(y=cons_levels['AMBUSDT'][2], color='red', linestyle='--', label='Level 3')
+    axs[1][2].axhline(y=cons_levels['AMBUSDT'][3], color='green', linestyle='--', label='Level 4')
+    
+    axs[1][3].plot(data8.index, data8[6], label='lpt', color='green')
+    axs[1][3].axhline(y=cons_levels['KNCUSDT'][0], color='red', linestyle='--', label='Level 1')
+    axs[1][3].axhline(y=cons_levels['KNCUSDT'][1], color='green', linestyle='--', label='Level 2')
+    axs[1][3].axhline(y=cons_levels['KNCUSDT'][2], color='red', linestyle='--', label='Level 3')
+    axs[1][3].axhline(y=cons_levels['KNCUSDT'][3], color='green', linestyle='--', label='Level 4')
+
+
+    axs[2][0].plot(data9.index, data9[6], label='lpt', color='green')
+    axs[2][0].axhline(y=cons_levels['LEVERUSDT'][0], color='red', linestyle='--', label='Level 1')
+    axs[2][0].axhline(y=cons_levels['LEVERUSDT'][1], color='green', linestyle='--', label='Level 2')
+    axs[2][0].axhline(y=cons_levels['LEVERUSDT'][2], color='red', linestyle='--', label='Level 3')
+    axs[2][0].axhline(y=cons_levels['LEVERUSDT'][3], color='green', linestyle='--', label='Level 4')
+    
+    axs[2][1].plot(data10.index, data10[6], label='lpt', color='green')
+    axs[2][1].axhline(y=cons_levels['MKRUSDT'][0], color='red', linestyle='--', label='Level 1')
+    axs[2][1].axhline(y=cons_levels['MKRUSDT'][1], color='green', linestyle='--', label='Level 2')
+    axs[2][1].axhline(y=cons_levels['MKRUSDT'][2], color='red', linestyle='--', label='Level 3')
+    axs[2][1].axhline(y=cons_levels['MKRUSDT'][3], color='green', linestyle='--', label='Level 4')
+    
+    axs[2][2].plot(data11.index, data11[6], label='lpt', color='green')
+    axs[2][2].axhline(y=cons_levels['PENDLEUSDT'][0], color='red', linestyle='--', label='Level 1')
+    axs[2][2].axhline(y=cons_levels['PENDLEUSDT'][1], color='green', linestyle='--', label='Level 2')
+    axs[2][2].axhline(y=cons_levels['PENDLEUSDT'][2], color='red', linestyle='--', label='Level 3')
+    axs[2][2].axhline(y=cons_levels['PENDLEUSDT'][3], color='green', linestyle='--', label='Level 4')
+    
+    axs[2][3].plot(data12.index, data12[6], label='lpt', color='green')
+    axs[2][3].axhline(y=cons_levels['SPELLUSDT'][0], color='red', linestyle='--', label='Level 1')
+    axs[2][3].axhline(y=cons_levels['SPELLUSDT'][1], color='green', linestyle='--', label='Level 2')
+    axs[2][3].axhline(y=cons_levels['SPELLUSDT'][2], color='red', linestyle='--', label='Level 3')
+    axs[2][3].axhline(y=cons_levels['SPELLUSDT'][3], color='green', linestyle='--', label='Level 4')
 
     # Set titles
-    axs[0].set_title('BTC')
-    axs[1].set_title('ARKM')
-    axs[2].set_title('ACH')
+    axs[0][0].set_title('BTC')
+    axs[0][1].set_title('ARKM')
+    axs[0][2].set_title('ACH')
+    axs[0][3].set_title('LPT')
+    
+    axs[1][0].set_title('STORJ')
+    axs[1][1].set_title('WLD')
+    axs[1][2].set_title('AMB')
+    axs[1][3].set_title('KNC')
+    
+    axs[2][0].set_title('LEVER')
+    axs[2][1].set_title('MKR')
+    axs[2][2].set_title('PENDLE')
+    axs[2][3].set_title('SPELL')
 
 # Create the animation
-anim = FuncAnimation(fig, update, init_func=init, frames=np.arange(0, 100), interval=10000)  # 300000 ms = 5 minutes
+anim = FuncAnimation(fig, update, init_func=init, frames=np.arange(0, 100), interval=300000)  # 300000 ms = 5 minutes
 
 # Show the plot
 plt.tight_layout()
